cc27308855cceb6b833113eff0405045628da6a1
WilliamCWH <William.Chen@redcypress.com.tw>
2019-12-24 16:39:41 +0800

    update

--- cocos-slot-test/assets/script/Test.js
+++ cocos-slot-test/assets/script/Test.js
@@ -1,17 +1,16 @@
 var Roller = require("slot.Roller");
+var AudioManager = require("AudioManager");
 cc.Class({
   extends: cc.Component,
   properties: {
-    // foo: {
-    //    default: null,      // The default value will be used only when the component attaching
-    //                           to a node for the first time
-    //    url: cc.Texture2D,  // optional, default is typeof default
-    //    serializable: true, // optional, default is true
-    //    visible: true,      // optional, default is true
-    //    displayName: 'Foo', // optional
-    //    readonly: false,    // optional, default is false
-    // },
-    // ...
+    audioManager: {
+      type: AudioManager,
+      default: null
+    },
+    rewardNum: {
+      type: cc.EditBox,
+      default: null
+    }
   },
 
   // use this for initialization
@@ -23,45 +22,63 @@ cc.Class({
     rollers.forEach(function(roller, index, array) {
       roller.toggleSpin();
     });
+    this.audioManager.playSpinning();
   },
 
   w_spin: function() {
     var rollers = this.node.getComponentsInChildren(Roller);
     var delay = 1000;
+    var num_string = this.rewardNum.string;
+    var res = num_string.split("");
+    console.log("res = " + res.length);
+    console.log("res 0  = " + res[0]);
+    console.log("res 0  = " + res[1]);
+    console.log("res 0  = " + res[2]);
+    console.log("res 0  = " + res[3]);
+    console.log("res 0  = " + res[4]);
+    // var num = parseInt(num_string);
+    // console.log("num = " + num);
 
     setTimeout(
       function() {
-        rollers[0].toggleSpin();
+        rollers[0].toggleSpin(parseInt(res[4]));
+        this.audioManager.playMetal();
       }.bind(this),
       delay + 2000
     );
 
     setTimeout(
       function() {
-        rollers[1].toggleSpin();
+        rollers[1].toggleSpin(parseInt(res[3]));
+        this.audioManager.playMetal();
+      }.bind(this),
+      delay + 3000
+    );
+
+    setTimeout(
+      function() {
+        rollers[2].toggleSpin(parseInt(res[2]));
+        this.audioManager.playMetal();
       }.bind(this),
       delay + 4000
     );
 
     setTimeout(
       function() {
-        rollers[2].toggleSpin();
+        rollers[3].toggleSpin(parseInt(res[1]));
+        this.audioManager.playMetal();
       }.bind(this),
-      delay + 6000
+      delay + 4500
     );
 
     setTimeout(
       function() {
-        rollers[3].toggleSpin();
+        rollers[4].toggleSpin(parseInt(res[0]));
+        this.audioManager.playMetal();
+        this.audioManager.playWinSound();
+        this.audioManager.pauseSpinning();
       }.bind(this),
-      delay + 8000
-    );
-
-    setTimeout(
-      function() {
-        rollers[4].toggleSpin();
-      }.bind(this),
-      delay + 10000
+      delay + 6500
     );
 
     // for (var i = 0; i < this.rollers + 1; i++) {
